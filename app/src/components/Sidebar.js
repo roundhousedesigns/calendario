8105eee11d184317582f572a6ffb4e2f24e871ee
Nick Gaswirth <nick@roundhouse-designs.com>
2021-01-08 15:00:28 -0800

    even dumber

--- app/src/components/Sidebar.js
+++ app/src/components/Sidebar.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { routeBase } from "../lib/utils.js";
 
 export default function Sidebar() {
