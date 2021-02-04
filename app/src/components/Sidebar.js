8d461b5e27dc23db52b78ded33377927f2399701
Nick Gaswirth <nick@roundhouse-designs.com>
2021-02-03 22:06:31 -0500

    outlining layout

--- app/src/components/Sidebar.js
+++ app/src/components/Sidebar.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Sidebar() {
-	return <aside className="calendario__sidebar">Sidebar</aside>;
+	return (
+		<aside className="calendario__sidebar">
+			<div className="widget">
+				<h3 className="widget-title">Unscheduled Drafts</h3>
+			</div>
+			<div className="widget">
+				<h3 className="widget-title">At a Glance</h3>
+			</div>
+		</aside>
+	);
 }
