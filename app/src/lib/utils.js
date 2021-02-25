b4f10efb7a308d89d973c932afe1e875723c586d
Nick Gaswirth <nick@roundhouse-designs.com>
2021-02-25 16:54:22 -0500

    removed comments

--- app/src/lib/utils.js
+++ app/src/lib/utils.js
@@ -35,49 +35,6 @@ export const postStatuses = window.rhdReactPlugin.postStatuses; // Live
 // 	},
 // };
 
-export const samplePosts = {
-	scheduled: [
-		{
-			id: 1,
-			post_title: "Test Post 1",
-			post_status: "draft",
-			post_date: "02-15-2021", // will be more accurate and have post time, as well
-		},
-		{
-			id: 2,
-			post_title: "Test Post 2",
-			post_status: "future",
-			post_date: "02-13-2021", // will be more accurate and have post time, as well
-		},
-		{
-			id: 6,
-			post_title: "Test Post 6",
-			post_status: "future",
-			post_date: "02-14-2021", // will be more accurate and have post time, as well
-		},
-	],
-	unscheduled: [
-		{
-			id: 3,
-			post_title: "Test Post 3",
-			post_status: "draft",
-			post_date: "02-16-2021", // will be more accurate and have post time, as well
-		},
-		{
-			id: 4,
-			post_title: "Test Post 4",
-			post_status: "pending",
-			post_date: "02-22-2021", // will be more accurate and have post time, as well
-		},
-		{
-			id: 5,
-			post_title: "Test Post 5",
-			post_status: "pending",
-			post_date: "02-22-2021", // will be more accurate and have post time, as well
-		},
-	],
-};
-
 export const dateFormat = {
 	day: "d",
 	// date: "MM-dd-yyyy",
