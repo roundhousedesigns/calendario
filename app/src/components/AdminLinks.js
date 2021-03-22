cabe3a030563d889076a798df9aa41d3bf680dd6
Nick Gaswirth <nick@roundhouse-designs.com>
2021-03-22 15:09:58 -0400

    styles

---
+++ app/src/components/AdminLinks.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { wp } from "../lib/utils";
+
+export default function AdminLinks() {
+	const {
+		wpLinks: { blogUrl, trashUrl },
+	} = wp;
+
+	const renderLink = ([link, title, target]) => {
+		target = target ? target : "_self";
+
+		return (
+			<li>
+				<a rel="noreferrer" href={link} target={target}>
+					{title} <span className="icon">open_in_new</span>
+				</a>
+			</li>
+		);
+	};
+
+	return (
+		<ul className="adminLinks">
+			{renderLink([trashUrl, "Trash", "_blank"])}
+			{renderLink([blogUrl, "View Posts", "_blank"])}
+		</ul>
+	);
+}
