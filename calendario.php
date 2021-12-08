c1592b8f649cb165b4fd590354728e2127c5d47d
Nick Gaswirth <nick@roundhouse-designs.com>
2021-12-07 20:03:24 -0500

    CI/CD

--- calendario.php
+++ calendario.php
@@ -21,11 +21,11 @@ define( 'RHD_CALENDARIO_PLUGIN_DIR_BASE_URL', plugin_dir_url( __FILE__ ) );
 
 // Load JS interface locally.
 // phpcs:disable Squiz.Commenting.InlineComment.InvalidEndChar
-define( 'RHD_CALENDARIO_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) . 'app/' );
-define( 'RHD_CALENDARIO_REACT_APP_BUILD', RHD_CALENDARIO_PLUGIN_DIR_URL . 'build/' );
+// define( 'RHD_CALENDARIO_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) . 'app/' );
+// define( 'RHD_CALENDARIO_REACT_APP_BUILD', RHD_CALENDARIO_PLUGIN_DIR_URL . 'build/' );
 
 // Load JS interface from CloudFlare Pages (CI/CD).
-// define( 'RHD_CALENDARIO_REACT_APP_BUILD', 'https://calendario.roundhouse-designs.com/' );
+define( 'RHD_CALENDARIO_REACT_APP_BUILD', 'https://calendario.roundhouse-designs.com/' );
 
 // phpcs:enable Squiz.Commenting.InlineComment.InvalidEndChar
 
