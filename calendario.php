c3bf6f0645a4c3fec940925409707522cb932527
Nick Gaswirth <nick@roundhouse-designs.com>
2022-01-13 12:08:29 -0500

    Working deps again

--- calendario.php
+++ calendario.php
@@ -5,7 +5,7 @@
  * Author:         Roundhouse Designs
  * Author URI:     https://roundhouse-designs.com
  * Text Domain:    rhd
- * Version:        0.8.01
+ * Version:        0.8.02
  *
  * @package       calendario
  *
@@ -15,7 +15,7 @@
 /**
  * Paths.
  */
-define( 'RHD_CALENDARIO_PLUGIN_VERSION', '0.8.01' );
+define( 'RHD_CALENDARIO_PLUGIN_VERSION', '0.8.02' );
 define( 'RHD_CALENDARIO_PLUGIN_DIR_BASE', plugin_dir_path( __FILE__ ) );
 define( 'RHD_CALENDARIO_PLUGIN_DIR_BASE_URL', plugin_dir_url( __FILE__ ) );
 
