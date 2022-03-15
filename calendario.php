2dcdb0be5e2fd5bf0f011cc5eb2d928f9cc24d53
Nick Gaswirth <nick@roundhouse-designs.com>
2022-03-15 09:48:57 -0400

    CI/CD

--- calendario.php
+++ calendario.php
@@ -19,12 +19,11 @@ define( 'RHD_CALENDARIO_PLUGIN_VERSION', '1.0.1-dev' );
 define( 'RHD_CALENDARIO_PLUGIN_DIR_BASE', plugin_dir_path( __FILE__ ) );
 define( 'RHD_CALENDARIO_PLUGIN_DIR_BASE_URL', plugin_dir_url( __FILE__ ) );
 
-// Load JS interface locally.
-// define( 'RHD_CALENDARIO_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) . 'app/' );
-// define( 'RHD_CALENDARIO_REACT_APP_BUILD', RHD_CALENDARIO_PLUGIN_DIR_URL . 'build/' );
-
-// Load JS interface from CloudFlare Pages (CI/CD).
+// CloudFlare Pages (CI/CD).
 define( 'RHD_CALENDARIO_REACT_APP_BUILD', 'https://calendario.roundhouse-designs.com/' );
+
+// Load JS interface locally.
+// define( 'RHD_CALENDARIO_REACT_APP_BUILD_LOCAL', plugin_dir_url( __FILE__ ) . 'app/build/' );
 
 /**
  * Base configuration.
